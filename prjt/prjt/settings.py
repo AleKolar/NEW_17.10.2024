5152a06b49828472665a350d7cc96b88c211761b
AleKolar <157892507+AleKolar@users.noreply.github.com>
2024-10-18 19:32:46 +0300

    Update settings.py

--- prjt/prjt/settings.py
+++ prjt/prjt/settings.py
@@ -77,20 +77,6 @@ WSGI_APPLICATION = 'prjt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'app11',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT':5432,
-#         'OPTIONS': {
-#             'options': '-c client_encoding=UTF8 -c search_path=public'
-#         },
-#     }
-# }
-
 load_dotenv()
 
 DATABASES = {
