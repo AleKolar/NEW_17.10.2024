2c6603e85af7ab6bc88cb750e057e9548aae0905
AleKolar <gefest-173@rambler.ru>
2024-10-20 13:55:19 +0300

    coverage_tests_1

--- app11/tests.py
+++ app11/tests.py
@@ -8,40 +8,37 @@ from .serializers import PerevalAddedSerializer
 
 class ModelsTestCase(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith',
+                                        phone='123456789')
+        self.coords = Coords.objects.create(latitude='40.7128', longitude='74.0060', height=100)
+        self.level = Level.objects.create(winter='cold', summer='hot', autumn='cool', spring='warm')
+        self.pereval = PerevalAdded.objects.create(beauty_title='Beautiful', title='Title', other_titles='Others',
+                                                   connect='Connection', user=self.user, coords=self.coords,
+                                                   level=self.level, status='new')
+        self.image = Images.objects.create(data='ImageData', title='ImageTitle', pereval=self.pereval)
+
     def test_user_creation(self):
-        user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith', phone='123456789')
-        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(self.user.email, 'test@example.com')
 
     def test_coords_creation(self):
-        coords = Coords.objects.create(latitude='40.7128', longitude='74.0060', height=100)
-        self.assertEqual(coords.height, 100)
+        self.assertEqual(self.coords.height, 100)
 
     def test_level_creation(self):
-        level = Level.objects.create(winter='cold', summer='hot', autumn='cool', spring='warm')
-        self.assertEqual(level.spring, 'warm')
+        self.assertEqual(self.level.spring, 'warm')
 
     def test_perevaladded_creation(self):
-        user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith', phone='123456789')
-        coords = Coords.objects.create(latitude='40.7128', longitude='74.0060', height=100)
-        level = Level.objects.create(winter='cold', summer='hot', autumn='cool', spring='warm')
-        pereval = PerevalAdded.objects.create(beauty_title='Beautiful', title='Title', other_titles='Others',
-                                              connect='Connection', user=user, coords=coords, level=level, status='new')
-        self.assertEqual(pereval.status, 'new')
+        self.assertEqual(self.pereval.status, 'new')
 
     def test_images_creation(self):
-        user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith', phone='123456789')
-        coords = Coords.objects.create(latitude='40.7128', longitude='74.0060', height=100)
-        level = Level.objects.create(winter='cold', summer='hot', autumn='cool', spring='warm')
-        pereval = PerevalAdded.objects.create(beauty_title='Beautiful', title='Title', other_titles='Others',
-                                              connect='Connection', user=user, coords=coords, level=level, status='new')
-        image = Images.objects.create(data='ImageData', title='ImageTitle', pereval=pereval)
-        self.assertEqual(image.title, 'ImageTitle')
+        self.assertEqual(self.image.title, 'ImageTitle')
 
 
 class PerevalAddedSerializerTestCase(TestCase):
 
     def setUp(self):
-        self.user_data = {'email': 'test@test.com', 'phone': '1234567890', 'fam': 'Doe', 'name': 'John', 'otc': 'Smith'}
+        self.user_data = {'email': 'test@test.com', 'phone': '1234567890', 'fam': 'Doe', 'name': 'John',
+                          'otc': 'Smith'}
         self.coords_data = {'latitude': 40.7128, 'longitude': -74.0060}
         self.level_data = {'name': 'Beginner'}
         self.images_data = [{'data': 'image1.png', 'title': 'Image 1'}, {'data': 'image2.png', 'title': 'Image 2'}]
@@ -69,12 +66,14 @@ class PerevalAddedSerializerTestCase(TestCase):
 class PerevalAddedViewSetTestCase(APITestCase):
 
     def setUp(self):
-        self.user = User.objects.create(email='test@test.com', phone='1234567890', fam='Doe', name='John', otc='Smith')
+        self.user = User.objects.create(email='test@test.com', phone='1234567890', fam='Doe', name='John',
+                                        otc='Smith')
         self.coords = Coords.objects.create(latitude=40.7128, longitude=-74.0060)
         self.level = Level.objects.create(name='Beginner')
         self.image1 = Images.objects.create(data='image1.png', title='Image 1')
         self.image2 = Images.objects.create(data='image2.png', title='Image 2')
-        self.pereval = PerevalAdded.objects.create(status='Active', url='example.com', beauty_title='Beautiful Title',
+        self.pereval = PerevalAdded.objects.create(status='Active', url='example.com',
+                                                   beauty_title='Beautiful Title',
                                                    title='Title', other_titles='Other Titles', connect='Connected',
                                                    user=self.user, coords=self.coords, level=self.level)
         self.pereval.images.add(self.image1, self.image2)
@@ -86,7 +85,8 @@ class PerevalAddedViewSetTestCase(APITestCase):
 
     def test_create_perevaladded(self):
         data = {'status': 'Active', 'url': 'example.com', 'beauty_title': 'Beautiful Title', 'title': 'Title',
-                'other_titles': 'Other Titles', 'connect': 'Connected', 'user': self.user.pk, 'coords': self.coords.pk,
+                'other_titles': 'Other Titles', 'connect': 'Connected', 'user': self.user.pk,
+                'coords': self.coords.pk,
                 'level': self.level.pk, 'images': [self.image1.pk, self.image2.pk]}
         url = reverse('perevaladded-list')
         response = self.client.post(url, data, format='json')
@@ -104,18 +104,14 @@ class PerevalAddedViewSetTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_highest_pereval(self):
-        url = reverse(
-            'highest_pereval')
+        url = reverse('highest_pereval')
         response = self.client.get(url)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
     def test_highest_pereval_no_coords(self):
-        url = reverse(
-            'highest_pereval')
+        url = reverse('highest_pereval')
         response = self.client.get(url)
-
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(response.data, {"message": "No Coords objects found"})
 
@@ -144,3 +140,19 @@ class TestAPIEndpoints(APITestCase):
         url = reverse('create')
         response = self.client.post(url, data={})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class DatabaseConnectionTestCase(TestCase):
+
+    def test_database_connection(self):
+        from django.db import connection
+        self.assertIsNotNone(connection)
+
+    def test_write_to_database(self):
+        user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith', phone='123456789')
+        self.assertIsNotNone(user.id)
+
+    def test_read_from_database(self):
+        user = User.objects.create(email='test@example.com', fam='Doe', name='John', otc='Smith', phone='123456789')
+        retrieved_user = User.objects.get(id=user.id)
+        self.assertEqual(user.email, retrieved_user.email)
